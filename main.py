9a911570d1f5d46f30f7043156f4d62f6b305bbb
zsozsi9512 <102908557+zsozsi9512@users.noreply.github.com>
2022-04-09 14:48:31 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from score_board import ScoreBoard
+
+screen=Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+screen = Screen()
+scoreboard = ScoreBoard()
+
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.update_score()
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or  snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+
+    #Detect collision with own tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+    # Detect collision with own tail (SOLUTION NR. 2)
+    # for segment in snake.segments[1:]:
+    #     if snake.head.distance(segment) < 10:
+    #         game_is_on = False
+    #         scoreboard.game_over()
+
+
+
+
+screen.exitonclick()
+
